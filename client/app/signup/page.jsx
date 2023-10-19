051f4df6859e58a34e1bdaba96c2f97b44247ae7
chadcat7 <namishpande2007@gmail.com>
2023-10-19 15:19:48 +0530

    up

--- client/app/signup/page.jsx
+++ client/app/signup/page.jsx
@@ -107,7 +107,7 @@ const SignPage = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-primary px-3 py-2.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
               >
-                Sign in
+                Sign Up
               </button>
             </div>
           </form>
